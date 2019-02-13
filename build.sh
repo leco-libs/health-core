4830c91d48fe939b0597b865c651101c86c7e669
leco <lcosta.sp.br@gmail.com>
2019-02-13 09:01:14 -0200

    change information about bintray

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
 # SQL
-export DATABASE_DRIVER="org.postgresql.Driver"
-export DATABASE_URL="jdbc:postgresql://localhost:5432/health"
-export DATABASE_USERNAME="sa"
-export DATABASE_PASSWORD="sa"
+export DATABASE_DRIVER="org.postgresql.Driver" && \
+export DATABASE_URL="jdbc:postgresql://localhost:5432/health" && \
+export DATABASE_USERNAME="sa" && \
+export DATABASE_PASSWORD="sa" && \
 # NOSQL
-export NOSQL_DATABASE_HOSTNAME="localhost"
-export NOSQL_DATABASE_NAME="health"
-export NOSQL_DATABASE_USERNAME="sa"
-export NOSQL_DATABASE_PASSWORD="sa"
+export NOSQL_DATABASE_HOSTNAME="localhost" &&\
+export NOSQL_DATABASE_NAME="health" &&\
+export NOSQL_DATABASE_USERNAME="sa" &&\
+export NOSQL_DATABASE_PASSWORD="sa" &&\
 
-export BINTRAY_KEY="c6890ca3d54aa153b255ef5f2e704ce482632e67"
-export BINTRAY_USER="l3cobr"
-
-./gradlew clean build -x test
+./gradlew clean build -x test &&\
 
 ./gradlew bintrayUpload
